66234efce24d2d704d14cdd20f377ef5be57c51d
Sergio Florez-Orrego <saf9215@stern.nyu.edu>
2025-12-18 15:37:19 -0500

    Add batch job to run

--- scripts/build_Cij_PCs/slurm/build_Cij_panel.sh
+++ scripts/build_Cij_PCs/slurm/build_Cij_panel.sh
@@ -8,4 +8,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # bash env should be the project bash_env, with project-specific uploads for pythonenv
 
 source "$PROJECT_ROOT_PATH/bash_env.sh"
-python -u "${SCRIPT_DIR}/master.py" 2>&1 | tee "${SCRIPT_DIR}/0_master_py.log"
+python -u "${SCRIPT_DIR}/../build.py" 2>&1 | tee "${SCRIPT_DIR}/../build.log"
--- scripts/build_Cij_PCs/slurm/build_Cij_panel_params.sh
+++ scripts/build_Cij_PCs/slurm/build_Cij_panel_params.sh
@@ -5,16 +5,17 @@
 # === Resource allocation ===
 NODES=1
 NTASKS_PER_NODE=1
-CPUS_PER_TASK=4
-TIME="1:00:00"
-MEM="18G"
+CPUS_PER_TASK=28
+TIME="3:00:00"
+MEM="160G"
 PARTITION="general"
+ACCOUNT="torch_pr_366_general"
 
 # === Job parameters ===
-PROJECT="default"
+PROJECT="build_Cij_panel"
 JOB_NAME="${PROJECT}_job"
 JOB_OUTPUT="${PROJECT}_job.out"
-JOB_SCRIPT="batch_job_simplified_default.sh"
+JOB_SCRIPT="build_Cij_panel.sh"
 
 # === Notifications ===
 MAIL_USER="saf9215@stern.nyu.edu"
