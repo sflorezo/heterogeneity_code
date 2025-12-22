719860624948252a258a3b9b3123e0892afd0101
Sergio Florez-Orrego <saf9215@stern.nyu.edu>
2025-12-22 15:52:30 -0500

    debug

--- scripts/build_Cij_PCs/slurm/build_Cij_panel.sh
+++ scripts/build_Cij_PCs/slurm/build_Cij_panel.sh
@@ -3,12 +3,16 @@
 # Simplified batch job default example
 # ============================================================
 
+# source env_machine without output to get master paths
+
+source "$HOME/.env_machine" >/dev/null 2>&1
+
 # manually define paths to bash_env and job to run
 
-PROJECT_ROOT_PATH="/storage/Dropbox/01_main/dev/heterogeneity_code"
-SCRIPT_DIR="$PROJECT_ROOT_PATH/scripts/build_Cij_PCs/build.py"
+BASH_ENV_PATH="$HETEROGENEITY_CODE_PATH/bash_env.sh"
+RUN_SCRIPT_DIR="$HETEROGENEITY_CODE_PATH/scripts/build_Cij_PCs"
 
 # bash env should be the project bash_env, with project-specific uploads for pythonenv
 
-source "$PROJECT_ROOT_PATH/bash_env.sh"
-python -u "${SCRIPT_DIR}/build.py" 2>&1 | tee "${SCRIPT_DIR}/build.log"
+source "$BASH_ENV_PATH"
+python -u "${RUN_SCRIPT_DIR}/build.py" 2>&1 | tee "${RUN_SCRIPT_DIR}/build.log"
