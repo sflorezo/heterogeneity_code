67234bc83e3ccc9a1ed215eea410d8d923845780
Sergio Florez-Orrego <saf9215@stern.nyu.edu>
2025-12-22 15:27:06 -0500

    debug

--- bash_env.sh
+++ bash_env.sh
@@ -19,45 +19,45 @@ source $HOME/.env_machine
 
 # initialize_micromamba
 
-type initialize_micromamba
+declare -F
 
 
-micromamba activate heterogeneity_code
+# micromamba activate heterogeneity_code
 
-# ========== project level globals ========== #
+# # ========== project level globals ========== #
 
-# DO NOT USE SCRIPTDIR. THIS FILE WILL BE RUN FROM DIFFERENT FILES,
-# SO PLEASE CALL GLOBALS ALREADY DEFINED IN MASTER ENVIRONMENT
+# # DO NOT USE SCRIPTDIR. THIS FILE WILL BE RUN FROM DIFFERENT FILES,
+# # SO PLEASE CALL GLOBALS ALREADY DEFINED IN MASTER ENVIRONMENT
 
-export PROJECT_ROOT="$HETEROGENEITY_CODE_PATH"
-export PROJECT_SRC="$PROJECT_ROOT/src"
-export SEC_API_KEY="$SEC_API_KEY"
-export DEEPSEEK_API_KEY="$DEEPSEEK_API_KEY"
+# export PROJECT_ROOT="$HETEROGENEITY_CODE_PATH"
+# export PROJECT_SRC="$PROJECT_ROOT/src"
+# export SEC_API_KEY="$SEC_API_KEY"
+# export DEEPSEEK_API_KEY="$DEEPSEEK_API_KEY"
 
-# ========== expand configs for python ========== #
+# # ========== expand configs for python ========== #
 
-mkdir -p configs_local
-expanded_toml=$(envsubst < $PROJECT_ROOT/configs/project_params.toml)
-printf "%s" "$expanded_toml" > $PROJECT_ROOT/configs_local/project_params.toml
+# mkdir -p configs_local
+# expanded_toml=$(envsubst < $PROJECT_ROOT/configs/project_params.toml)
+# printf "%s" "$expanded_toml" > $PROJECT_ROOT/configs_local/project_params.toml
 
-# ========== project level aliases ========== #d
+# # ========== project level aliases ========== #d
 
-# alias cd_documents='cd $DATA_RAW/factset/documents'
-# alias cd_symbology='cd $DATA_TEMP/factset/symbology'
-# alias cd_ownership='cd $DATA_RAW/factset/ownership'
+# # alias cd_documents='cd $DATA_RAW/factset/documents'
+# # alias cd_symbology='cd $DATA_TEMP/factset/symbology'
+# # alias cd_ownership='cd $DATA_RAW/factset/ownership'
 
-# CLEAN: I think this is no longer needed as the packages are included in installed packages on the conda env
-# # ---- PYTHONPATH
-# export PYTHONPATH="${PROJECT_ROOT}:${PROJECT_SRC}:${PYSFO_SRC}:${PYTHONPATH}"
+# # CLEAN: I think this is no longer needed as the packages are included in installed packages on the conda env
+# # # ---- PYTHONPATH
+# # export PYTHONPATH="${PROJECT_ROOT}:${PROJECT_SRC}:${PYSFO_SRC}:${PYTHONPATH}"
 
-# ========== work variables ========== #d
+# # ========== work variables ========== #d
 
-export_envfile "$PROJECT_ROOT/.vscode/.env" \
-    PROJECT_ROOT \
-    SEC_API_KEY \
-    DEEPSEEK_API_KEY
+# export_envfile "$PROJECT_ROOT/.vscode/.env" \
+#     PROJECT_ROOT \
+#     SEC_API_KEY \
+#     DEEPSEEK_API_KEY
 
-# ========== env activation assetion ========== #
+# # ========== env activation assetion ========== #
 
-echo "[heterogeneity_code] Environment heterogeneity_code activated succesfully"
+# echo "[heterogeneity_code] Environment heterogeneity_code activated succesfully"
 
