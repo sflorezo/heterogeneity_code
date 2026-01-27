f9a13ee6751b4a487881a5b363c9d3b2a8adf7b8
Sergio Florez-Orrego <saf9215@stern.nyu.edu>
2026-01-27 11:37:53 -0500

    debug

--- scripts/a_build_Cij_PCs/slurm/build_Cij_panel.sh
+++ scripts/a_build_Cij_PCs/slurm/build_Cij_panel.sh
@@ -10,7 +10,7 @@ source "$HOME/.env_machine" >/dev/null 2>&1
 # manually define paths to bash_env and job to run
 
 BASH_ENV_PATH="$HETEROGENEITY_CODE_PATH/bash_env.sh"
-RUN_SCRIPT_DIR="$HETEROGENEITY_CODE_PATH/scripts/build_Cij_PCs"
+RUN_SCRIPT_DIR="$HETEROGENEITY_CODE_PATH/scripts/a_build_Cij_PCs"
 
 # bash env should be the project bash_env, with project-specific uploads for pythonenv
 
