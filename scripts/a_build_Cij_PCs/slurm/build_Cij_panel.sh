940e46c2985627d15b0eba822d18e2e23c57e44f
Sergio Florez-Orrego <saf9215@stern.nyu.edu>
2026-01-27 17:35:12 -0500

    misc

--- scripts/a_build_Cij_PCs/slurm/build_Cij_panel.sh
+++ scripts/a_build_Cij_PCs/slurm/build_Cij_panel.sh
@@ -15,5 +15,4 @@ RUN_SCRIPT_DIR="$HETEROGENEITY_CODE_PATH/scripts/a_build_Cij_PCs"
 # bash env should be the project bash_env, with project-specific uploads for pythonenv
 
 source "$BASH_ENV_PATH"
-# python -u "${RUN_SCRIPT_DIR}/1_build_portf_weights.py" 2>&1 | tee "${RUN_SCRIPT_DIR}/1_build_portf_weights.log"
-python -u "${RUN_SCRIPT_DIR}/2_build_Cij_PCs.py" 2>&1 | tee "${RUN_SCRIPT_DIR}/2_build_Cij_PCs.log"
+python -u "${RUN_SCRIPT_DIR}/build_Cij_PCs.py" 2>&1 | tee "${RUN_SCRIPT_DIR}/build_Cij_PCs.log"
