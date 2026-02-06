ce8a00dee14a226329f88af6c70efb6f8f7e182c
Sergio Florez-Orrego <saf9215@stern.nyu.edu>
2026-02-06 15:29:17 -0500

    misc

--- src/heterogeneity_code/zx_scratch/main.py
+++ src/heterogeneity_code/zx_scratch/main.py
@@ -1,19 +1,6 @@
 #%% ========== configs ========== %%#
 
-from heterogeneity_code.a_configs import CONFIGS
-from pysfo.basic import load_parquet
-from heterogeneity_code.d_nport_portshares.a_build_PCs.b_build_port_weights.b_build_port_weights import _keep_bond_funds
-from heterogeneity_code.d_nport_portshares.b_check_PCs.a_preliminary.a_merge_PCs_and_funds import fetch_PCs_with_fund_info
+import heterogeneity_code.a_configs 
 import heterogeneity_code.b_bond_prices
-import statsmodels.api as sm
-import numpy as np
-import matplotlib.pyplot as plt
-
-# from pysfo.basic import *
-
-PROCESSED_NPORT = CONFIGS["PATHS"]["PROCESSED_NPORT"]
-
-#%% ========== go ========== %%#
-
-fund_info = load_parquet(PROCESSED_NPORT / "NPORT_funds_allQuarters.parquet")
+import heterogeneity_code.d_nport_portshares
 
